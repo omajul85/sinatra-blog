06966a5c451c6bc7293c0cf0bacb7729e34dd6d6
Omar <omajul2@hotmail.com>
2016-11-05 16:22:05 +0000

    Initial commit

---
+++ lib/blog.rb
@@ -0,0 +1,36 @@
+require 'sinatra/base'
+require 'ostruct'
+require 'github_hook'
+require 'time'
+require 'slim'
+require 'yaml'
+
+class Blog < Sinatra::Base
+  use GithubHook
+
+  set :root, File.expand_path('../../', __FILE__)
+  set :articles, []
+  set :app_file, __FILE__
+
+  Dir.glob "#{root}/articles/*.md" do |file|
+    meta, content = File.read(file).split("\n\n", 2)
+    article = OpenStruct.new YAML.load(meta)
+    article.date = Time.parse article.date.to_s
+    article.content = content
+    article.slug = File.basename(file, 'md')
+
+    get "/#{article.slug}" do
+      slim :post, locals: { article: article }
+    end
+
+    articles << article
+  end
+
+  # Sort articles by date, display new articles first
+  articles.sort_by! { |article| article.date }
+  articles.reverse!
+
+  get "/" do
+    slim :index
+  end
+end
